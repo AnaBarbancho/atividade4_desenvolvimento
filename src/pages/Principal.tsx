085c9a57c5743106ee4f10428afaf75436084bab
LAB-80 <ana.afonso2@fatec.sp.gov.br>
2024-04-30 21:25:02 -0300

    atividade em aula

---
+++ src/pages/Principal.tsx
@@ -0,0 +1,26 @@
+
+import Megasena from "../components/Megasena";
+import Quina from "../components/Quina";
+import Timemania from "../components/Timemania";
+import { useLoteria } from "../hooks";
+import "./index.css";
+
+export default function Principal() {
+  const { megasena } = useLoteria();
+
+  return (
+    <>
+      {megasena.dataApuracao ? (
+        <div className="principal-bloco">
+          <Megasena />
+          <Quina />
+          <Timemania />
+        </div>
+      ) : (
+        <div className="principal-carregando">
+          <h3>Carregando...</h3>
+        </div>
+      )}
+    </>
+  );
+}
